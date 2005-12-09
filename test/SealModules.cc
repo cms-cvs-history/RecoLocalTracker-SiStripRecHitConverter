c813983cf92a3ed1534d675005fe2479ae5689da
Tommaso Boccali <sha1-ce6f8e33294befdbdd970525cd264b612743e399@cern.ch>
2005-12-09 10:19:18 +0000

    producer moved to analyzer

--- test/SealModules.cc
+++ test/SealModules.cc
@@ -3,9 +3,10 @@
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-#include "RecoLocalTracker/ReadRecHit/interface/ReadRecHit.h"
+#include "RecoLocalTracker/SiStripRecHitConverter/test/ReadRecHit.h"
 
 using cms::ReadRecHit;
+
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(ReadRecHit)
 
