3cc5112ad77e182948448ecfd239bd413469f857
Patrizia Azzi <sha1-23a389c18865c7a952754e6c0904f9c1813da38e@cern.ch>
2006-04-10 10:07:47 +0000

    declares ValHit analyzer

--- test/SealModules.cc
+++ test/SealModules.cc
@@ -4,9 +4,12 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "RecoLocalTracker/SiStripRecHitConverter/test/ReadRecHit.h"
+#include "RecoLocalTracker/SiStripRecHitConverter/test/ValHit.h"
 
 using cms::ReadRecHit;
+using cms::ValHit;
 
 DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(ValHit)
 DEFINE_ANOTHER_FWK_MODULE(ReadRecHit)
 
